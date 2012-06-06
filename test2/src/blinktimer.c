2c1e5cfc688f4793e90a86cd7b8bb90373877a4e
modulastic <guillem.alegret@gmail.com>
2012-06-06 14:51:39 +0200

    3 timers working

--- test2/src/blinktimer.c
+++ test2/src/blinktimer.c
@@ -1,40 +1,39 @@
-#define F_CPU 16000000UL
+#define F_CPU 16000000UL // CM-510 runs at 16 MHz.
+
 #include <avr/io.h>
-//#include <util/delay.h>
 #include <avr/interrupt.h>
 
-void blinktimer(void){
-	DDRC |= 1<<4; // set Manage LED as output
-	DDRC |= 1<<5; // set Manage LED as output
-	DDRC |= 1<<6; // set Manage LED as output
+/** Example on how to setup a timer on the CM510 (Atmega2561) */
 
-	TCCR1B = (1<<CS11)|(1<<CS10);		//
-	OCR1A = 12400;
-	//OCR1B = 1240;
-	//OCR1C = 12400;
+void blinktimer() {
+	DDRC |= 1<<4; 						// set Manage LED as output
+	DDRC |= 1<<5; 						// set Manage LED as output
+	DDRC |= 1<<6; 						// set Manage LED as output
 
-	TIMSK1 = (1<<OCIE1A);//|(1<<OCIE1B)|(1<<OCIE1C); // Enable three interrupts
-	TCNT1 = 0; // Reset counter
-	sei();      // enable global interrupts
 
+	TCCR1B = (1<<CS12);//|(1<<CS10); 	//256 Prescaler
+	OCR1A = 1240;
+	OCR1B = 5000;
+	OCR1C = 10000,
+	TIMSK1 = (1<<OCIE1A)|(1<<OCIE1B)|(1<<OCIE1C);//Enable Output Compare Match Interrupt
+	TCNT1 = 0; 							//reset timer/counter 1
+	sei(); 								//Enables global interrupts
 	while(1) {}
-	//return 0;
+	return 0;
 }
 
-
-// Interrupts
-IRS(TIMER1_COMPA_vect) {
+ISR(TIMER1_COMPA_vect) {
 	PORTC ^= 1 << 4; // swap LED
-	OCR1A += 124;
-}
-/*
-IRSB(TIMER1_COMPB_vect) {
-	PORTC ^= 1 << 5; // swap LED
-	OCR1B += 1240;
+	OCR1A += 1240; //
 }
 
-IRSC(TIMER1_COMPC_vect) {
-	PORTC ^= 1 << 6; // swap LED
-	OCR1C += 12400;
+ISR(TIMER1_COMPB_vect) {
+	PORTC ^= 1 << 5; // swap LED
+	OCR1B += 5000;
 }
-*/
+
+ISR(TIMER1_COMPC_vect) {
+	PORTC ^= 1 << 6; // swap LED
+	OCR1C += 10000;
+}
+
