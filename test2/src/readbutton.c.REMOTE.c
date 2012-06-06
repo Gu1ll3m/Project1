24c1dd877a4a5682c826690c6d149297bb1cb16f
modulastic <guillem.alegret@gmail.com>
2012-06-06 13:32:45 +0200

    deleat trash

--- test2/src/readbutton.c.REMOTE.c
+++
@@ -1,9 +0,0 @@
-#define F_CPU 16000000UL
-#include <avr/io.h>
-#include <util/delay.h>
-
-int readbutton(int pin){
-	//DDRE &= ~(1<<pin); // set button pin as input
-	int bit = PINE & (1<<pin);//this is the comment i added, Balint:)!!!
-	return bit;
-}
