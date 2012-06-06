4b6cac4b849b42bf68a31f127d722504fd244d71
faridx <farid_sattari@hotmail.com>
2012-06-06 12:05:53 +0200

    comment

---
+++ test2/src/readbutton.c.LOCAL.c
@@ -0,0 +1,9 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+// Farid added comment
+int readbutton(int pin){
+	DDRE &= ~(1<<pin); // set button pin as input
+	int bit = PINE & (1<<pin);
+	return bit;
+}
