f55c7126bf5fd791077ed9b04b3e1e935a87c0d8
modulastic <guillem.alegret@gmail.com>
2012-06-06 13:47:18 +0200

    read button fix

--- test2/src/readbutton.c
+++ test2/src/readbutton.c
@@ -1,11 +1,12 @@
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
 int readbutton(int pin){
 	DDRE &= ~(1<<pin); // set button pin as input
 	int bit = PINE & (1<<pin);
 	DDRE &= ~(1<<pin); // set button pin as input
-	int bit = PINE & (1<<pin);
+	bit = PINE & (1<<pin);
 	return bit;
 }
