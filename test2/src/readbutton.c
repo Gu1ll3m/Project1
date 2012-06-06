914c7f1571aae84e1f8445e41383555c212ed2f5
modulastic <guillem.alegret@gmail.com>
2012-06-06 13:34:39 +0200

    fix the read botton

--- test2/src/readbutton.c
+++ test2/src/readbutton.c
@@ -3,11 +3,9 @@
 #include <util/delay.h>
 
 int readbutton(int pin){
-<<<<<<< HEAD
 	DDRE &= ~(1<<pin); // set button pin as input
 	int bit = PINE & (1<<pin);
 	DDRE &= ~(1<<pin); // set button pin as input
 	int bit = PINE & (1<<pin);
->>>>>>> 1c31f118829cd037da4c605db1be661284dc62db
 	return bit;
 }
