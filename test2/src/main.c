4edb3803201221e18d0bb2ab3b55b1b28e036a16
modulastic <guillem.alegret@gmail.com>
2012-06-06 17:27:38 +0200

    New start

--- test2/src/main.c
+++ test2/src/main.c
@@ -10,31 +10,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "test.h"
-#include "blink.h"
-#include "manblink.h"
-#include "blinktimer.h"
-#include "gserialtest.h"
+
+// Robitis API
+#include "serial.h"
+//#include "zigbee.h"
+//#include "zgb_hal.h"
+//#include "dynamixel.h"
+//#include "dxl_hal.h"
+
+// OUR API
+//#include "button.h"
+//#include "led.h"
+//#include "pc.h"
+//#include "adc.h"
+//#include "irlong.h"
+//#include "irshort.h"
+//#include "touch.h"
+//#include "gyro.h"
+//#include "motor.h"
+
 
 int main(void) {
-	//cm510_init();
-	//controller_init();
-
-    // Hello World printing and using header file
-	//printme();
-
-	// LED blinking 0,5 Hz
-    //blink();
-
-	//Button controller LEDs
-	//manblink();
-
-	// Timer controlled LEDs
-	//blinktimer();
-
-	// Test with the serial
-	gserialtest();
-
 
 
 	return 0;
